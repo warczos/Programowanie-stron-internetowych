df80252bf47f1b3fb294698c5f33840266c4b965
warczos <olik3007@gmail.com>
2022-04-11 23:23:00 +0200

    63

--- Programowanie stron internetowych/cw24/aktualizacja.js
+++ Programowanie stron internetowych/cw24/aktualizacja.js
@@ -1,11 +1,11 @@
-function aktualizacja() {
-  var d,m,r,g,min,s;
-  d=11;
-  m=04;
-  r=2022;
-  g=23;
-  min=10;
-  s=36;
+function updatetime() {
+  var d, m, r, g, min, s;
+  d = 11;
+  m = 04;
+  r = 2022;
+  g = 23;
+  min = 10;
+  s = 36;
 
-  document.getElementById("i").innerHTML=d+"/"+m+"/"+r;
+  document.getElementById("akt").innerHTML = d + "/" + m + "/" + r;
 }
