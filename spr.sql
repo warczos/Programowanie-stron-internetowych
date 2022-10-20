697e64ee410d7a201b81ff3c86526a4bfa622d7c
warczos <olik3007@gmail.com>
2022-10-20 09:47:09 +0200

    83

---
+++ spr.sql
@@ -0,0 +1,5 @@
+SELECT (stawka_godzinowa-stawka_godzinowa*0.2359)*168 AS 'Pensja' from egzaminatorzy;
+SELECT 3*99*1.08 AS 'Koszt egzaminów'; 320,76
+SELECT AVG(stawka_godzinowa) FROM egzaminatorzy;
+SELECT MAX(stawka_godzinowa) FROM egzaminatorzy;
+SELECT 
